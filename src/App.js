06e0d8ee8267d2a591aa5b79e75298f606b19195
mre55 <misganawmihreteab>
2022-01-25 02:14:21 +0300

    Remove provider until store data is created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import NavBar from './components/NavBar';
 import Missions from './components/Missions';
 import Rockets from './components/Rockets';
 import MyProfile from './components/MyProfile';
-import store from './redux/storeConfig';
 
 const App = () => (
-  <Provider store={store}>
+  <div>
+    <NavBar />
     <div>
-      <NavBar />
-      <div>
-        <Routes>
-          <Route path="/" element={<Rockets />} />
-          <Route path="/missions" element={<Missions />} />
-          <Route path="/my-profile" element={<MyProfile />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+      </Routes>
     </div>
-  </Provider>
+  </div>
 );
 
 export default App;
