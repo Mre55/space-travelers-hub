1de8715cc6fdba6f46ba9498c93884c1abb5ad2b
williamrolando88 <william.rolando88@hotmail.com>
2022-01-24 15:52:18 -0500

    Add Mission component

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import Mission from './Mission';
 
-const Missions = () => <div>Here comes missions!</div>;
+const Missions = () => {
+  return (
+    <div>
+      <div className="grid">
+        <div className="grid grid-cols-4">
+          <div>Mission</div>
+          <div>Description</div>
+          <div>Status</div>
+          <div>Action</div>
+        </div>
+        <Mission />
+        <Mission />
+        <Mission />
+        <Mission />
+      </div>
+    </div>
+  );
+};
 
 export default Missions;
