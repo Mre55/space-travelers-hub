26ba63f1b0b3d5a6f217cc3db8ef73d5b796e823
williamrolando88 <william.rolando88@hotmail.com>
2022-01-24 18:54:45 -0500

    Add missions reducer and actions

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,5 +1,46 @@
-// Constants
+// *Constants
+const FETCH = 'FETCH';
+const STORE = 'STORE';
 
-// Actions
+// *Actions
+// Fetch data from API
+export const fetchingData = () => async (dispatch) => {
+  const data = await fetch('https://api.spacexdata.com/v3/missions', {
+    method: 'GET',
+  });
+  try {
+    const missions = await data.json();
+    dispatch(storeMissions(missions));
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-// Reducer
+// Store data
+const storeMissions = (payload) => {
+  return {
+    type: STORE,
+    payload,
+  };
+};
+
+// *Reducer function
+const missionsReducer = (state = [], payload) => {
+  switch (payload.type) {
+    case STORE:
+      return [
+        payload.payload.map((mission) => {
+          return {
+            title: mission.mission_name,
+            description: mission.description,
+            id: mission.mission_id,
+            reserved: false,
+          };
+        }),
+      ];
+    default:
+      return state;
+  }
+};
+
+export default missionsReducer;
