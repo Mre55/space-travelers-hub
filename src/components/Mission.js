d5c61618c399b2983bada32001822ff9bef88c8c
williamrolando88 <william.rolando88@hotmail.com>
2022-01-24 16:46:06 -0500

    Pass data to child as prop

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,24 +1,16 @@
 import React from 'react';
 
 const Mission = (props) => {
-  // const {
-  //   mission: { title, description, status },
-  // } = props;
+  const {
+    mission: { title, description, member },
+  } = props;
 
   return (
     <div className="grid grid-cols-4">
-      <div>Title</div>
+      <div>{title}</div>
+      <div>{description}</div>
       <div>
-        Description Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Excepturi accusantium aspernatur quos debitis, officiis pariatur
-        temporibus quae voluptates impedit assumenda itaque necessitatibus
-        molestiae incidunt asperiores dolores veritatis, provident quaerat
-        doloribus harum similique alias est dicta ratione modi. Repellendus
-        temporibus, expedita ipsum enim, fuga, molestias ullam fugiat vel
-        commodi animi cum.
-      </div>
-      <div>
-        <div>Badge</div>
+        <div>{member ? 'Active member' : 'Not a member'}</div>
       </div>
       <div>
         <button>Join</button>
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -2,6 +2,32 @@ import React from 'react';
 import Mission from './Mission';
 
 const Missions = () => {
+  const missions = [
+    {
+      title: 'Title 1',
+      description:
+        'Description 1 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, id. Exercitationem ab, dolorum nostrum voluptate fuga architecto id dolore a quam voluptatibus quos perspiciatis iure porro tempore quod itaque dicta.',
+      member: false,
+    },
+    {
+      title: 'Title 2',
+      description:
+        'Description 2 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, id. Exercitationem ab, dolorum nostrum voluptate fuga architecto id dolore a quam voluptatibus quos perspiciatis iure porro tempore quod itaque dicta.',
+      member: true,
+    },
+    {
+      title: 'Title 3',
+      description:
+        'Description 3 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, id. Exercitationem ab, dolorum nostrum voluptate fuga architecto id dolore a quam voluptatibus quos perspiciatis iure porro tempore quod itaque dicta.',
+      member: false,
+    },
+    {
+      title: 'Title 4',
+      description:
+        'Description 4 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, id. Exercitationem ab, dolorum nostrum voluptate fuga architecto id dolore a quam voluptatibus quos perspiciatis iure porro tempore quod itaque dicta.',
+      member: false,
+    },
+  ];
   return (
     <div>
       <div className="grid">
@@ -11,10 +37,9 @@ const Missions = () => {
           <div>Status</div>
           <div>Action</div>
         </div>
-        <Mission />
-        <Mission />
-        <Mission />
-        <Mission />
+        {missions.map((mission, index) => (
+          <Mission key={index} mission={mission} />
+        ))}
       </div>
     </div>
   );
