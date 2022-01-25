10829ec402e9c9f0e1bc483d45917721898feeae
williamrolando88 <william.rolando88@hotmail.com>
2022-01-25 16:53:04 -0500

    Finish styling NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import logo from '../assets/planet.png';
 
 const NavBar = () => {
   const activeClass = (isActive) => {
-    let staticClass = 'bg-red-400';
+    let staticClass = 'mr-4 text-blue-500 hover:underline';
     if (isActive.isActive) {
       staticClass += ' underline';
     }
@@ -12,19 +12,26 @@ const NavBar = () => {
   };
 
   return (
-    <div>
-      <img src={logo} width={50} alt="Logo" />
-      <div>
-        <NavLink className={(isActive) => activeClass(isActive)} to="/">
-          Rockets
-        </NavLink>
-        <NavLink className={(isActive) => activeClass(isActive)} to="/missions">
-          Missions
-        </NavLink>
+    <div className="flex justify-between items-center shadow-md px-4 py-2">
+      <div className="flex items-center gap-4">
+        <img className="w-14" src={logo} width={50} alt="Logo" />
+        <h1 className="text-3xl">Space Travelers' HUB</h1>
+      </div>
+      <div className="flex items-center gap-4">
+        <div className="flex border-r-2 border-black">
+          <NavLink className={(isActive) => activeClass(isActive)} to="/">
+            Rockets
+          </NavLink>
+          <NavLink
+            className={(isActive) => activeClass(isActive)}
+            to="/missions">
+            Missions
+          </NavLink>
+        </div>
+
         <NavLink
           className={(isActive) => activeClass(isActive)}
-          to="/my-profile"
-        >
+          to="/my-profile">
           My Profile
         </NavLink>
       </div>
