23aded42a14cbf4508c61b4b9df28331d5cf66e2
williamrolando88 <william.rolando88@hotmail.com>
2022-01-25 17:12:08 -0500

    Add filter state to render reserver mission

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => <div>This is my profile</div>;
+const MyProfile = () => {
+  const missions = useSelector((state) => state.missionsReducer);
+
+  const missionsReserved =
+    missions[0] && missions[0].filter((mission) => mission.reserved === true);
+
+  return (
+    <div>
+      <div>
+        {missionsReserved &&
+          missionsReserved.map((mission) => (
+            <div key={mission.id}>{mission.title}</div>
+          ))}
+      </div>
+    </div>
+  );
+};
 
 export default MyProfile;
