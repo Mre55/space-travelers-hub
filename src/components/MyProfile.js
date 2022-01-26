ccafbaa310d5f1ee7a057b0a7813eec963f18299
williamrolando88 <william.rolando88@hotmail.com>
2022-01-26 10:17:21 -0500

    Fix border differences

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -5,25 +5,25 @@ const MyProfile = () => {
   const missions = useSelector((state) => state.missionsReducer);
   const rocketsFromStore = useSelector((state) => state.rocketsReducer);
 
-  const missionsReserved = missions[0] && missions[0].filter((m) => m.reserved === true);
-  const rocketReserved = rocketsFromStore[0]
-    && rocketsFromStore[0].filter((m) => m.reserve === true);
+  const missionsReserved =
+    missions[0] && missions[0].filter((m) => m.reserved === true);
+  const rocketReserved =
+    rocketsFromStore[0] &&
+    rocketsFromStore[0].filter((m) => m.reserve === true);
 
   return (
     <div className="p-10 grid grid-cols-2 gap-10">
       <div>
         <h2 className="text-3xl font-semibold px-4 py-2">My Missions</h2>
         <div
-          className={`flex flex-col border border-b-0 border-black ${
+          className={`flex flex-col border border-b-0 rounded-t-lg border-black ${
             missionsReserved ? '' : 'hidden'
-          }`}
-        >
-          {missionsReserved
-            && missionsReserved.map((mission) => (
+          }`}>
+          {missionsReserved &&
+            missionsReserved.map((mission) => (
               <div
                 className="border-b px-4 py-2 text-xl border-black"
-                key={mission.id}
-              >
+                key={mission.id}>
                 {mission.title}
               </div>
             ))}
@@ -32,16 +32,14 @@ const MyProfile = () => {
       <div>
         <h2 className="text-3xl font-semibold py-2">My Rockets</h2>
         <div
-          className={`flex flex-col border border-b-0 text-gray-600 border-gray-400 rounded-t-lg ${
+          className={`flex flex-col border border-b-0 border-black rounded-t-lg ${
             rocketReserved ? '' : 'hidden'
-          }`}
-        >
-          {rocketReserved
-            && rocketReserved.map((rocket) => (
+          }`}>
+          {rocketReserved &&
+            rocketReserved.map((rocket) => (
               <div
-                className="border-b px-4 py-2 text-xl border-gray-400"
-                key={rocket.id}
-              >
+                className="border-b px-4 py-2 text-xl border-black"
+                key={rocket.id}>
                 {rocket.rocketName}
               </div>
             ))}
