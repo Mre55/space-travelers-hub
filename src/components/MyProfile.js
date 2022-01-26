685d0ff98e72d454ca0736082cdb04b5d512b6fc
mre55 <misganawmihreteab>
2022-01-26 12:31:13 +0300

    Render a list of all reserved rockets on the "My profile" page.

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,5 +1,54 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => <div>This is my profile</div>;
+const MyProfile = () => {
+  const missions = useSelector((state) => state.missionsReducer);
+  const rocketsFromStore = useSelector((state) => state.rocketsReducer);
+
+  const missionsReserved = missions[0] && missions[0].filter((m) => m.reserved === true);
+  const rocketReserved = rocketsFromStore[0]
+    && rocketsFromStore[0].filter((m) => m.reserve === true);
+
+  return (
+    <div className="p-10 grid grid-cols-2 gap-10">
+      <div>
+        <h2 className="text-3xl font-semibold px-4 py-2">My Missions</h2>
+        <div
+          className={`flex flex-col border border-b-0 border-black ${
+            missionsReserved ? '' : 'hidden'
+          }`}
+        >
+          {missionsReserved
+            && missionsReserved.map((mission) => (
+              <div
+                className="border-b px-4 py-2 text-xl border-black"
+                key={mission.id}
+              >
+                {mission.title}
+              </div>
+            ))}
+        </div>
+      </div>
+      <div>
+        <h2 className="text-3xl font-semibold py-2">My Rockets</h2>
+        <div
+          className={`flex flex-col border border-b-0 text-gray-600 border-gray-400 rounded-t-lg ${
+            rocketReserved ? '' : 'hidden'
+          }`}
+        >
+          {rocketReserved
+            && rocketReserved.map((rocket) => (
+              <div
+                className="border-b px-4 py-2 text-xl border-gray-400"
+                key={rocket.id}
+              >
+                {rocket.rocketName}
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default MyProfile;
