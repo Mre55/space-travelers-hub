4d848f546fe341d0f3260a512d67e15726e1a660
mre55 <misganawmihreteab>
2022-01-26 07:58:44 +0300

    Switch badges for Rockets - Conditional components when it is booked and not booked

--- src/components/ListRockets.js
+++ src/components/ListRockets.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { reserveRocket, cancelReservation } from '../redux/rockets/rocketsReducer';
+import {
+  reserveRocket,
+  cancelReservation,
+} from '../redux/rockets/rocketsReducer';
 
 const ListRockets = (props) => {
   const {
@@ -23,23 +26,43 @@ const ListRockets = (props) => {
   return (
     <div>
       <div className="flex space-x-10">
-        <div><img className="flex-none w-80" src={flickrImages} alt="Rocket" /></div>
+        <div>
+          <img className="flex-none w-80" src={flickrImages} alt="Rocket" />
+        </div>
         <div className="flex-none flex flex-col space-y-4 w-8/12">
           <div>
             <div className="text-3xl">{rocketName}</div>
           </div>
           <div>
-            <div className="bg-emerald-400 text-xs text-white p-2 rounded w-24">{reserve ? 'Reserved' : 'Not reserved'}</div>
+            {reserve ? (
+              <div>
+                <div className="bg-emerald-400 text-xs text-white p-2 m-1 rounded w-16">
+                  Reserved
+                </div>
+                <div>{description}</div>
+              </div>
+            ) : (
+              description
+            )}
           </div>
-          <div>{description}</div>
           <div>
-            <button
-              onClick={handleReservation}
-              className="bg-blue-600 text-white p-3 rounded"
-              type="button"
-            >
-              {reserve ? 'Cancel Reservation' : 'Reserve Rocket'}
-            </button>
+            {reserve ? (
+              <button
+                onClick={handleReservation}
+                className="bg-inherit border-solid border-2 text-lg text-gray-500 border-gray-400 p-2 rounded"
+                type="button"
+              >
+                Cancel Reservation
+              </button>
+            ) : (
+              <button
+                onClick={handleReservation}
+                className="bg-blue-600 text-white p-2 w-40 text-lg  rounded"
+                type="button"
+              >
+                Reserve Rocket
+              </button>
+            )}
           </div>
         </div>
       </div>
