4586dd159bfbc547a5c17cc259583883b310cf0a
mre55 <misganawmihreteab>
2022-01-27 13:29:51 +0300

    Add `describe` method to wrap the dispatch actions tests

--- src/components/__test__/Rockets.test.js
+++ src/components/__test__/Rockets.test.js
@@ -1,58 +1,68 @@
-import configureStore from 'redux-mock-store';
-import renderer from 'react-test-renderer';
-import { Provider } from 'react-redux';
+import configureStore from "redux-mock-store";
+import renderer from "react-test-renderer";
+import { Provider } from "react-redux";
 
-import { reserveRocket, storeRockets, cancelReservation } from '../../redux/rockets/rocketsReducer';
-import Rockets from '../__mock__/Rockets';
+import {
+  reserveRocket,
+  storeRockets,
+  cancelReservation,
+} from "../../redux/rockets/rocketsReducer";
+import Rockets from "../__mock__/Rockets";
 
-const middlewares = [];
-const mockStore = configureStore(middlewares);
+describe("Dispatch actions tests", () => {
+  const middlewares = [];
+  const mockStore = configureStore(middlewares);
 
-it('should dispatch Reserve Rocket action', () => {
-  const initialState = {};
-  const store = mockStore(initialState);
+  it("should dispatch Reserve Rocket action", () => {
+    const initialState = {};
+    const store = mockStore(initialState);
 
-  // Dispatch the action
-  store.dispatch(reserveRocket());
+    // Dispatch the action
+    store.dispatch(reserveRocket());
 
-  // Test if your store dispatched the expected actions
-  const actions = store.getActions();
-  const expectedPayload = { type: 'RESERVE_ROCKET' };
-  expect(actions).toEqual([expectedPayload]);
+    // Test if your store dispatched the expected actions
+    const actions = store.getActions();
+    const expectedPayload = { type: "RESERVE_ROCKET" };
+    expect(actions).toEqual([expectedPayload]);
+  });
+
+  it("should dispatch cancel Reservation action", () => {
+    const initialState = {};
+    const store = mockStore(initialState);
+
+    // Dispatch the action
+    store.dispatch(cancelReservation());
+
+    // Test if your store dispatched the expected actions
+    const actions = store.getActions();
+    const expectedPayload = { type: "CANCEL_ROCKET" };
+    expect(actions).toEqual([expectedPayload]);
+  });
+
+  it("should dispatch store Rockets action", () => {
+    const initialState = {};
+    const store = mockStore(initialState);
+
+    // Dispatch the action
+    store.dispatch(storeRockets());
+
+    // Test if your store dispatched the expected actions
+    const actions = store.getActions();
+    const expectedPayload = { type: "FETCH_ROCKET" };
+    expect(actions).toEqual([expectedPayload]);
+  });
 });
 
-it('should dispatch cancel Reservation action', () => {
-  const initialState = {};
-  const store = mockStore(initialState);
-
-  // Dispatch the action
-  store.dispatch(cancelReservation());
-
-  // Test if your store dispatched the expected actions
-  const actions = store.getActions();
-  const expectedPayload = { type: 'CANCEL_ROCKET' };
-  expect(actions).toEqual([expectedPayload]);
-});
-
-it('should dispatch store Rockets action', () => {
-  const initialState = {};
-  const store = mockStore(initialState);
-
-  // Dispatch the action
-  store.dispatch(storeRockets());
-
-  // Test if your store dispatched the expected actions
-  const actions = store.getActions();
-  const expectedPayload = { type: 'FETCH_ROCKET' };
-  expect(actions).toEqual([expectedPayload]);
-});
-
-describe('AddTodo snapshot tests', () => {
+describe("Rockets component snapshot tests", () => {
   const mockStore = configureStore();
 
-  it('renders correctly', () => {
+  it("renders correctly", () => {
     const tree = renderer
-      .create(<Provider store={mockStore({})}><Rockets /></Provider>)
+      .create(
+        <Provider store={mockStore({})}>
+          <Rockets />
+        </Provider>
+      )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
